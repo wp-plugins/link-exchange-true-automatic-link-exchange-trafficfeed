3b974eb7b78e3218cb139389d52e6637832bb517
trafficfeed <trafficfeed@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-06 08:47:36 +0000

    Getting started added
    git-svn-id: https://plugins.svn.wordpress.org/link-exchange-true-automatic-link-exchange-trafficfeed/trunk@1128571 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ getting-started.php
@@ -0,0 +1,30 @@
+<div class="wrap">
+    <h2>Trafficfeed Getting Started</h2>
+    <h3>How to use this plugin?</h3>
+    <ol>
+        <li>
+           Register for a free trafficfeed.com account from within plugin. Your site will be added automatically once you register.
+        </li>
+        <li>
+           Then you need to go to your wordpress installation(Appearance -> Widgets) and add Trafficfeed Widget to  the place where you would like links to appear. (Sidebar or Footer recommended ). And if you are familiar with the system you can add the proper code anywhere you would like links to appear.
+        </li>
+        <li>
+            Then you need to login into your trafficfeed account and add pages that you want to use for link window or link directory.
+        </li>
+        <li>
+            Start using the system, GO TO “Exchange by Catalogue” and find link partners.
+        </li>
+        <li>
+            GO TO “Auto Exchange Settings” add sites and pages you would like to participate. (auto exchange is enabled by default)
+        </li>
+         <li>
+            For more details please visit <a href="http://www.trafficfeed.com?ref=wp_getting_started" target="_blank" title="trafficfeed.com">TrafficFeed.com</a>
+        </li>
+    </ol>  
+    <div>
+        <h3>For better understanding do watch this video.</h3>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/FPcGAxvVw0o" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
